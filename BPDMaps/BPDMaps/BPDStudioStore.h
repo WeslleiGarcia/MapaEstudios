6a859ba9d08248a527bdd4d94f1b8ad01a5d43a4
Wesllei <Garcia@MacBook-Pro-de-Wesllei.local>
2015-05-13 01:03:19 -0400

    criando BPDStudioStore

---
+++ BPDMaps/BPDMaps/BPDStudioStore.h
@@ -0,0 +1,13 @@
+//
+//  BPDStudioStore.h
+//  BPDMaps
+//
+//  Created by Wesllei on 13/05/15.
+//  Copyright (c) 2015 Wesllei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BPDStudioStore : NSObject
+
+@end
---
+++ BPDMaps/BPDMaps/BPDStudioStore.m
@@ -0,0 +1,13 @@
+//
+//  BPDStudioStore.m
+//  BPDMaps
+//
+//  Created by Wesllei on 13/05/15.
+//  Copyright (c) 2015 Wesllei. All rights reserved.
+//
+
+#import "BPDStudioStore.h"
+
+@implementation BPDStudioStore
+
+@end
