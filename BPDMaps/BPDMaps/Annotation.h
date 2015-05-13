1b5f1bf644b84b1c035eb4397e7cbdfaf3935506
Wesllei <Garcia@MacBook-Pro-de-Wesllei.local>
2015-05-13 01:05:57 -0400

    criando classe ANNOTATION

---
+++ BPDMaps/BPDMaps/Annotation.h
@@ -0,0 +1,13 @@
+//
+//  Annotation.h
+//  BPDMaps
+//
+//  Created by Wesllei on 13/05/15.
+//  Copyright (c) 2015 Wesllei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Annotation : NSObject
+
+@end
---
+++ BPDMaps/BPDMaps/Annotation.m
@@ -0,0 +1,13 @@
+//
+//  Annotation.m
+//  BPDMaps
+//
+//  Created by Wesllei on 13/05/15.
+//  Copyright (c) 2015 Wesllei. All rights reserved.
+//
+
+#import "Annotation.h"
+
+@implementation Annotation
+
+@end
